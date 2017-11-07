a603cb35f288c3999ade4ab1043915139763b202
Mingyi <zhangmingyi@kedacom.com>
2017-11-07 12:04:01 +0800

    .

--- scripts/cerbero/gstreamer-1.12.3/build.sh
+++ scripts/cerbero/gstreamer-1.12.3/build.sh
@@ -6,25 +6,29 @@ function cerbero(){
 }
 
 commit='-b 0.1'
-
-#cerbero --load-cerbero  "${commit}"  &&
-#cerbero --load-cerbero-pkg-src &&
-#cerbero -c config/win64.cbc bootstrap
 version=1.12.3
+arch=x86_64
+platform=windows
+[ "x$(uname)" == "xLinux" } && platform=linux
+
+__home__=$(cerbero --cerbero-home-directory)
+cerbero --load-cerbero  "${commit}"  &&
+cerbero --load-cerbero-pkg-src &&
+cerbero -c config/win64.cbc bootstrap
+
 release_repo=$__root__/releases
-[ ! -d  $release_repo ] && mkdir -p $release_repo
 
-#cerbero -c config/win64.cbc cpm-pack --build-tools --type package --output-dir $release_repo
-#cerbero -c config/win64.cbc cpm-pack pkg-config --build-tools --output-dir $release_repo
+build_tool_repo=$release_repo/gstreamer-build_tools-$platform-$arch-version
+[ ! -d  $build_tool_repo ] && mkdir -p $build_tool_repo
 
-build_tool_repo=$release_repo/gstreamer-$version-win64-build_tools
-#[ ! -d  $build_tool_repo ] && mkdir -p $build_tool_repo
-#
-#cerbero -c config/win64.cbc cpm-pack pkg-config --build-tools --output-dir $build_tool_repo
-#cerbero -c config/win64.cbc cpm-pack --type package --build-tools --output-dir $build_tool_repo
+cerbero -c config/win64.cbc cpm-pack --type package --build-tools  --output-dir $build_tool_repo
+cerbero -c config/win64.cbc cpm-pack pkg-config      --build-tools --output-dir $build_tool_repo
 
+if [ $platform == "windows" ]; then
+    mv $__home__/build/build-tools $__home__/build/build-tools.origin
+fi
 
-#cerbero -c config/win64.cbc cpm-install gstreamer-build-tools-windows-x86_64-1.12.3.tar.bz2 --build-tools --repo $build_tool_repo
+cerbero -c config/win64.cbc cpm-install build_tools-$platform-$arch-$version.tar.bz2 --build-tools --repo $build_tool_repo
 
 cerbero -c config/win64.cbc package base --tarball
 
