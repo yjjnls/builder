b1a2f4f0aea72de4ddf83dff5755dcdc99cf7426
yjjnls <x-jj@foxmail.com>
2017-11-16 14:06:53 +0800

    .

--- commands/build/gstreamer1.12.3.sh
+++ commands/build/gstreamer1.12.3.sh
@@ -35,15 +35,13 @@ cbc=config/${cbc}.cbc
 #
 #
 #
-echo -e "
-HOME : $home
-config : $__config__
-cerbero config : $cbc
-"
+# echo -e "
+# HOME : $home
+# config : $__config__
+# cerbero config : $cbc
+# "
 #
 cerbero=$( $__libdir__/cerbero -c $cbc -i $__config__)
-echo '-----------------------'
-echo $cerbero
 #
 # clear previous build
 #
@@ -55,15 +53,14 @@ build_tools_prefix=$($cerbero.get_config build_tools_prefix)
 [ -d $build_tools_prefix ] && rm -rf $build_tools_prefix
 
 pkgname=$($cerbero.build_tools_pkg_name)
-echo '~~~~~~~~~~~~~'
 repo=$($cerbero.release_repo build_tools)
-echo -e "
-prefix : $prefix
-build_tools_prefix : $build_tools_prefix
-pkgname : $pkgname
-repo : $repo
+# echo -e "
+# prefix : $prefix
+# build_tools_prefix : $build_tools_prefix
+# pkgname : $pkgname
+# repo : $repo
 
-"
+# "
 # echo $repo
 # exit 0
 #$cerbero.run bootstrap --cpm-install  --build-tools-disable
@@ -82,6 +79,10 @@ rdir=$($cerbero.release_dir )/$($cerbero.release_tag base)
 [ -d $rdir ] && rm -rf $rdir
 mkdir -p $rdir/tarball
 
+rdir=$($cerbero.release_dir )/$($cerbero.release_tag gstreamer)
+[ -d $rdir ] && rm -rf $rdir
+mkdir -p $rdir/tarball
+
 cache=$($cerbero.get_config home_dir)/$($cerbero.get_config cache_file )
 [ -f $cache ] && rm -rf $cache
 
@@ -89,5 +90,10 @@ $cerbero.run package base --tarball -o $rdir/tarball
 check "Build base"
 $cerbero.run cpm-pack base --type sdk --output-dir $rdir
 check "Pack base"
+
+$cerbero.run package base --tarball -o $rdir/tarball
+check "Build gstreamer"
+$cerbero.run cpm-pack base --type sdk --output-dir $rdir
+check "Pack gstreamer"
 
 
